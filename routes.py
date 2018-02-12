7b2e266c9b11bc3acb2458877e2ba2580936559d
benlegendj <benedictmwendwa47@gmail.com>
2018-02-12 22:32:18 +0300

    Update routes.py

--- routes.py
+++ routes.py
@@ -7,9 +7,6 @@ app = Flask(__name__)
 def home():
   return render_template('home.html')
 
-if __name__ == '__main__':
-  app.run(debug=True)
-
 @app.route('/about')
 def about():
   return render_template('about.html')
